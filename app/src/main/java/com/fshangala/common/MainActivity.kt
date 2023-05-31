1695a4fc6a3f3009a8dcf8e7e55fe6ab0608885e
fshangala <fshangala@gmail.com>
2023-05-31 05:29:18 +0200

    top manu fully configured

--- app/src/main/java/com/fshangala/common/MainActivity.kt
+++ app/src/main/java/com/fshangala/common/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.fshangala.common
 
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private var model: LamboViewModel? = null
@@ -15,6 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         model = ViewModelProvider(this)[LamboViewModel::class.java]
         sharedPref = getSharedPreferences("MySettings", MODE_PRIVATE)
+
+        model!!.getLatestRelease()
+        model!!.releaseVersionResponse.observe(this) {
+            val currentVersion = "v"+BuildConfig.VERSION_NAME
+            if(it!=""){
+                val update = JSONObject(JSONArray(it).getString(0))
+                if (update.getString("tag_name") != currentVersion) {
+                    openUpdate()
+                }
+            }
+        }
+        model!!.releaseVersionResponseError.observe(this) {
+            val currentVersion = "v"+BuildConfig.VERSION_NAME
+            //Log.d("UPDATE",it)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -29,5 +48,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.preferencesBtn -> {
+                openConfig()
+            }
+
+            R.id.reconnectBtn -> {
+                model!!.createConnection(sharedPref!!)
+            }
+
+            R.id.reloadBrowserBtn -> {
+                model!!.getRequest(sharedPref!!,"/betsite/")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openConfig(){
+        val intent = Intent(this,ConfigActivity::class.java)
+        startActivity(intent)
+    }
+    private fun openUpdate(){
+        val intent = Intent(this,UpdateActivity::class.java)
+        startActivity(intent)
     }
 }
